cca6a95ec4195a7c0ff0e50384b549a1c785ad2b
atul.sharma <atul.sharma.hs@gmail.com>
2023-10-21 18:51:58 +0530

    Task 2 - Load two CMS blocks on the checkout page

---
+++ app/code/Seepossible/Checkout/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/sidebar.html': 'Seepossible_Checkout/template/sidebar.html'
+        },
+    }
+};
---
+++ app/code/Seepossible/Checkout/view/frontend/web/js/view/checkout/shipping/additional-block.js
@@ -0,0 +1,12 @@
+define([
+    'uiComponent'
+
+], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Seepossible_Checkout/checkout/shipping/additional-block'
+        }
+    });
+});
